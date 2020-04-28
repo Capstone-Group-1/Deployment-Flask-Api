d8a5d3d3ea91c3b7d6482a8d0e26a8c4ea55a599
Justin Mckena <mckennaman123@gmail.com>
2020-04-27 21:54:12 -0400

    possible mysql 500s fix

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
+# Prod
 mysql = {
     "host": "JustinMckenna.mysql.pythonanywhere-services.com",
     "user": "JustinMckenna",
     "passwd": "password1!",
     "db": "JustinMckenna$mydb"
 }
+
+# Dev
+# mysql = {
+#     "host": "localhost:5000",
+#     "user": "root",
+#     "passwd": "",
+#     "db": "mydb"
+# }
